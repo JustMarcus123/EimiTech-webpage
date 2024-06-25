2dcd65d1801d7e267c6f8c3697b2ce80195c196f
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-26 00:18:30 +0530

    feat: setup browser router and daisyui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
-function App() {
-  return <>hello World</>;
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+const App: React.FC = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </>
+  )
 }
 
 export default App;
