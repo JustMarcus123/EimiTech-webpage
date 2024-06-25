4e2a787318e9ad6be0514516439028e4ea6bbfb5
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-06-26 00:46:12 +0530

    feat: coming soon ui

--- client/src/Pages/ComingSoon.tsx
+++ client/src/Pages/ComingSoon.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import image from "../assets/comingsoon.svg"
 
 const ComingSoon: React.FC = () => {
     return (
-        <div>ComingSoon</div>
+
+        <div className="flex flex-col items-center justify-center h-screen px-4 py-16 bg-base-200">
+            <img className='py-5 w-60 ' src={image} alt="Coming soon" />
+            <p> Under Construction, Coming soon.. 😊</p>
+        </div>
+
     )
 }
 export default ComingSoon
