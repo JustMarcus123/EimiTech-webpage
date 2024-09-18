9f9fe9cc7dfa64f7007e725fc4894986113cc0c9
MTouthang <53576865+MTouthang@users.noreply.github.com>
2024-09-18 21:33:28 +0530

    feat: Page not found

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-import ComingSoon from "./Components/ComingSoon";
+import NotFound from "./Components/NotFound";
 const App: React.FC = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/*" element={<ComingSoon />} />
+
+        <Route path="/*" element={<NotFound />} />
       </Routes>
 
     </>
