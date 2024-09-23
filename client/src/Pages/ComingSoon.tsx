d4afb042ebc30646a2e428ed8a8c391de8d538cd
JustMarcus123 <141229878+JustMarcus123@users.noreply.github.com>
2024-09-23 22:35:10 +0530

    testing push

---
+++ client/src/Pages/ComingSoon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import image from "../assets/comingsoon.svg"
+
+const ComingSoon: React.FC = () => {
+    return (
+
+        <div className="flex flex-col items-center justify-center h-screen px-4 py-16 bg-base-200">
+            <img className='py-5 w-60 ' src={image} alt="Coming soon" />
+            <p> Under Construction, Coming soon.. 😊jvnc klcn z.ˀv m</p>
+        </div>
+
+    )
+}
+export default ComingSoon
